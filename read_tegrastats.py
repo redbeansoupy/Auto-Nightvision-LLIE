5424d49a1c3351d5d4251624e7a3ffa846a028ab
redbeansoupy <victorialam2003@gmail.com>
2025-02-04 09:15:28 +0100

    fix math in read tegrastats

--- read_tegrastats.py
+++ read_tegrastats.py
@@ -27,15 +27,15 @@ ram_pretest = 0
 cpu_pretest = [0] * CPU_CORES
 gpu_pretest = 0
 
-for file_idx in range(1, len(sys.argv), 1):
-    ram_avg = 0
-    ram_max = 0
-    cpu_avg = [0] * CPU_CORES
-    cpu_freq = [0] * CPU_CORES
-    gpu_avg = 0
-    # gpu_freq = 0 # Not on Jetson Nano
-    # VDD stat is not available on jetson :(
+ram_avg = 0
+ram_max = 0
+cpu_avg = [0] * CPU_CORES
+cpu_freq = [0] * CPU_CORES
+gpu_avg = 0
+# gpu_freq = 0 # Not on Jetson Nano
+# VDD stat is not available on jetson :(
 
+for file_idx in range(1, len(sys.argv), 1):
     with open(sys.argv[file_idx], "r") as file:
         lines = file.readlines()
         num_lines = len(lines)
@@ -58,7 +58,6 @@ for file_idx in range(1, len(sys.argv), 1):
                 cpu_avg[core_idx] += cpu_usage[core_idx]
                 if idx == 0: cpu_freq[core_idx] = frequencies[core_idx]
                 
-
             # GPU
             gpu_avg += int(line[gpu_idx][:-1])
 
@@ -66,12 +65,12 @@ for file_idx in range(1, len(sys.argv), 1):
         ram_avg /= num_lines
         cpu_avg = [i / num_lines for i in cpu_avg]
         gpu_avg /= num_lines
-
+	
     # Output
     if len(sys.argv) == 3 and file_idx == 1:
     	# save for later
         ram_pretest = ram_avg
-        cpu_pretest = cpu_avg
+        cpu_pretest = cpu_avg.copy()
         gpu_pretest = gpu_avg
         print("========== v PRETEST TEGRASTATS v ==========")
     elif len(sys.argv) == 3 and file_idx == 2:
@@ -88,14 +87,14 @@ for file_idx in range(1, len(sys.argv), 1):
         else:
             print(", ", end="")
     print(f"Average GPU usage: {gpu_avg:.2f}%")
-    
-    print("========== PRETEST SUBTRACTED =========")
-    print(f"Average RAM usage: {(ram_avg):.2f}/{ram_max}")
-    print(f"Average CPU usage: [", end="")
-    for core in range(CPU_CORES):
-        print(f"{cpu_avg[core]:.2f}%/{cpu_freq[core]}", end="")
-        if core == CPU_CORES - 1: # If last
-            print("]")
-        else:
-            print(", ", end="")
-    print(f"Average GPU usage: {gpu_avg:.2f}%")
+
+print("========== PRETEST SUBTRACTED =========")
+print(f"Average RAM usage: {(ram_avg - ram_pretest):.2f}/{ram_max}")
+print(f"Average CPU usage: [", end="")
+for core in range(CPU_CORES):
+    print(f"{(cpu_avg[core] - cpu_pretest[core]):.2f}%", end="")
+    if core == CPU_CORES - 1: # If last
+        print("]")
+    else:
+        print(", ", end="")
+print(f"Average GPU usage: {(gpu_avg - gpu_pretest):.2f}%")
